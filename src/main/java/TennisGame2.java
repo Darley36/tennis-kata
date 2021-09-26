2b798fe15532346e74a113d49e6d782d409c2a73
Darley36 <darleybest@gmail.com>
2021-09-25 20:45:04 -0500

    TennisGame2 class refactoring completed

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,13 +1,13 @@
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
 public class TennisGame2 implements TennisGame
 {
-    public int scorePlayer1 = 0;
-    public int scorePlayer2 = 0;
-    public String player1Name = "";
-    public String player2Name = "";
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
+    private String player1Name;
+    private String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -17,76 +17,96 @@ public class TennisGame2 implements TennisGame
     public String getScore(){
         String score = "";
         score = searchForEquals(score);
+        List<Boolean> playerOptions = getPlayerOptions();
+        score = getPlayerOneScore(score, playerOptions);
+        score = getPlayerTwoScore(score, playerOptions);
+        score = getEqualityByPlayerOne(score, playerOptions);
+        score = getEqualityByPlayerTwo(score, playerOptions);
+        if(scoreGreaterThanFour()) {
+            int minusResult = scorePlayer1-scorePlayer2;
+            score = getAdvantage(score, resultOption(minusResult));
+        }
+        return score;
+    }
 
-        if (scorePlayer1 > 0 && scorePlayer2 ==0)
+    private String getEqualityByPlayerTwo(String score, List<Boolean> playerOptions) {
+        if (playerOptions.get(1) && playerOptions.get(3))
+        {
+            player1Name=playerPosition(scorePlayer1);
+            player2Name=playerPosition(scorePlayer2);
+            score = player1Name + "-" + player2Name;
+        }
+        return score;
+    }
+
+    private String getEqualityByPlayerOne(String score, List<Boolean> playerOptions) {
+        if (playerOptions.get(0) && playerOptions.get(2))
+        {
+            player1Name=playerPosition(scorePlayer1);
+            player2Name=playerPosition(scorePlayer2);
+            score = player1Name + "-" + player2Name;
+        }
+        return score;
+    }
+
+    private String getPlayerTwoScore(String score, List<Boolean> playerOptions) {
+        if (playerOptions.get(6) && playerOptions.get(7))
+        {
+            player2Name = playerPosition(scorePlayer2);
+            score = getBothScores("Love",player2Name);
+        }
+        return score;
+    }
+
+    private String getPlayerOneScore(String score, List<Boolean> playerOptions) {
+        if (playerOptions.get(4) && playerOptions.get(5))
         {
             player1Name = playerPosition(scorePlayer1);
 
             score = getBothScores(player1Name,"Love");
         }
-        if (scorePlayer2 > 0 && scorePlayer1 ==0)
-        {
-            player2Name = playerPosition(scorePlayer2);
-            score = getBothScores("Love",player2Name);
-        }
-        
-        if (scorePlayer1 > scorePlayer2 && scorePlayer1 < 4)
-        {
-            player1Name=playerPosition(scorePlayer1);
-            player2Name=playerPosition(scorePlayer2);
-            score = player1Name + "-" + player2Name;
-        }
-        if (scorePlayer2 > scorePlayer1 && scorePlayer2 < 4)
-        {
-            player1Name=playerPosition(scorePlayer1);
-            player2Name=playerPosition(scorePlayer2);
-            score = player1Name + "-" + player2Name;
-        }
-        if(scoreGreatherThanFour()) {
-            int minusResult = scorePlayer1-scorePlayer2;
-            //getAdvantage(minusResult);
-            if (scorePlayer1 > scorePlayer2 ) {
-                score = "Advantage player1";
-            }
-
-            if (scorePlayer2 > scorePlayer1 ) {
-                score = "Advantage player2";
-            }
-
-            if ((scorePlayer1 - scorePlayer2) >= 2) {
-                score = "Win for player1";
-            }
-            if ((scorePlayer2 - scorePlayer1) >= 2) {
-                score = "Win for player2";
-            }
-        }
         return score;
+    }
+
+    private List<Boolean> getPlayerOptions() {
+        List<Boolean> playerOptions = new ArrayList<>();
+        playerOptions.add(scorePlayer1 > scorePlayer2);
+        playerOptions.add(scorePlayer2 > scorePlayer1);
+        playerOptions.add(scorePlayer1 < 4);
+        playerOptions.add(scorePlayer2 < 4);
+        playerOptions.add(scorePlayer1 > 0);
+        playerOptions.add(scorePlayer2 == 0);
+        playerOptions.add(scorePlayer2 > 0);
+        playerOptions.add(scorePlayer1 == 0);
+        return playerOptions;
+    }
+
+    private String getAdvantage(String score, int option) {
+        String[] messageOptions = {"Advantage "+player1Name,"Advantage "+player2Name,"Win for player1","Win for player2"};
+        if (option == -1) {
+            return score;
+        }
+        return messageOptions[option];
+    }
+
+    private int resultOption(int minusResult){
+        List<Boolean> minusResultOptions = Arrays.asList(minusResult == 1,minusResult == -1,minusResult >= 2,minusResult <= -2);
+        return minusResultOptions.indexOf(true);
     }
 
     private String searchForEquals(String score) {
-        if (returnConditions() && scorePlayer1 < 4)
+        if (returnConditionsEquals())
         {
             score = getEquality(scorePlayer1);
         }
-        if (returnConditions() && scorePlayer1 >=3){
-            score = getEquality(3);
-        }
         return score;
     }
 
-    private boolean scoreGreatherThanFour(){
+    private boolean scoreGreaterThanFour(){
         return scorePlayer1 >= 4 || scorePlayer2 >=4;
     }
 
-    private String getAdvantage(int minusResult){
-        String score="";
-        if(minusResult == 1){
-            score = "Advantage player1";
-        }
-        return score;
-    }
-
-    private boolean returnConditions(){
+    private boolean returnConditionsEquals(){
         if(scorePlayer1 == scorePlayer2) return true;
         return false;
     }
@@ -97,34 +117,18 @@ public class TennisGame2 implements TennisGame
         return player1Name + "-" + player2Name;
     }
 
-    private String playerPosition(int Player) {
-        if (Player < 4) {
+    private String playerPosition(int player) {
+        if (player < 4) {
             List<String> score = Arrays.asList("Love","Fifteen","Thirty","Forty");
-            return score.get(Player);
+            return score.get(player);
         }
         return  null;
     }
 
     private String getEquality(int scorePlayer1) {
         String[] equalityOptions = {"Love-All","Fifteen-All","Thirty-All","Deuce"};
-        return equalityOptions[scorePlayer1];
-    }
-
-    public void SetP1Score(int number){
-        for (int i = 0; i < number; i++)
-        {
-            scorePlayer1++;
-        }
-            
-    }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            scorePlayer2++;
-        }
-            
+        if(scorePlayer1 < 3) return equalityOptions[scorePlayer1];
+        return "Deuce";
     }
 
     public void wonPoint(String player) {
